f90efc89795f62a90a544469d703c592ec2d2155
Bhavya M Shah <PCA84@server7.com>
2024-09-30 17:08:06 +0530

    implementing liquibase

--- demo/src/main/resources/db/changeLog/changelog-master.sql
+++ demo/src/main/resources/db/changeLog/changelog-master.sql
@@ -1,2 +1,14 @@
-
 --liquibase formatted sql
+
+--changeset Bhavyaa:115
+CREATE TABLE demo_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+
+--changeset Bhavyaa:116
+INSERT INTO demo_table (name, description) VALUES ('bavya', 'This is the first sample description.');
+INSERT INTO demo_table (name, description) VALUES ('Sample Name 2', 'This is the second sample description.');
+
+
